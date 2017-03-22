271a289cc5ef2c587168234702065065a339ec1f
Dominique Pizzie <d.pizzie@louisiana.edu>
2017-03-22 12:43:24 -0500

    Bug fixes and optimizations
    * Converted double quotes to single quotes where possible
    * made $installArray an explicit array with @() to handle 0 or 1 objects properly 
    * added "-Status" flag to "Write-Progress" to fix PowerShell v2 error of "Write-Progress at command pipeline 1"

--- installer.ps1
+++ installer.ps1
@@ -30,47 +30,47 @@ param (
 )
 
 if (!$update) {
-    $boxstarter = "http://boxstarter.org/package/nr/url?"
-    $installDirectory = ".\appInstalls"
+    $boxstarter = 'http://boxstarter.org/package/nr/url?'
+    $installDirectory = '.\appInstalls'
     ### Edit these URLs to your own install file
-    $chocoList = "https://gist.githubusercontent.com/DomPizzie/f16556b326d0bd19ede16bc586a9a124/raw/a47c518ff11092a320cffd8a18c9de12307958f1/VMSetup.txt"
-    $niniteList = "https://ninite.com/chrome-firefox-keepass2-notepadplusplus-peazip-putty-vscode-winscp/ninite.exe"
+    $chocoList = 'https://gist.githubusercontent.com/DomPizzie/f16556b326d0bd19ede16bc586a9a124/raw/a47c518ff11092a320cffd8a18c9de12307958f1/VMSetup.txt'
+    $niniteList = 'https://ninite.com/chrome-firefox-keepass2-notepadplusplus-peazip-putty-vscode-winscp/ninite.exe'
     $boxstarter = $boxstarter + $chocoList
     
     ### Use Boxstarter web installer to intialize Windows settings and install Chocolatey
-    Write-Host "Installing from boxstarter"
-    Start-Process -FilePath "iexplore.exe" -ArgumentList $boxstarter
-    Start-Sleep -Seconds 25
-    Read-Host -Prompt "Press Enter to continue"
+    Write-Host 'Installing from boxstarter'
+    Start-Process -FilePath 'iexplore.exe' -ArgumentList $boxstarter
+    Start-Sleep -Seconds 15
+    Read-Host -Prompt 'Press Enter to continue'
 
     ### Install from ninite.exe
     ### If ninite is not downloaded, go download and execute
-    Write-Host "Installing from ninite"
-    $niniteExe = Get-ChildItem -Path $HOME\Downloads\ -Filter "Ninite" | Sort-Object LastAccessTime -Descending | Select-Object -First 1
+    Write-Host 'Installing from ninite'
+    $niniteExe = Get-ChildItem -Path $HOME\Downloads\ -Filter 'Ninite' | Sort-Object LastAccessTime -Descending | Select-Object -First 1
     $niniteExe.FullName
     if (!$niniteExe) {
         $WebClient = New-Object System.Net.WebClient
-        $WebClient.DownloadFile("$niniteList","$HOME\Downloads\Ninite.exe")
+        $WebClient.DownloadFile($niniteList,"$HOME\Downloads\Ninite.exe")
         Start-Process -FilePath "$HOME\Downloads\Ninite.exe" -Wait
     } else {
         Start-Process -FilePath $niniteExe.FullName -Wait
     }
     
     ### Install all applications inside of included directory
-    Write-Host "Installing from appInstalls"
-    [string[]]$installArray = Get-ChildItem -Path $installDirectory -Name
+    Write-Host 'Installing from appInstalls'
+    [string[]]$installArray = @(Get-ChildItem -Path $installDirectory -Name)
     $count = 0
     if ($installArray) {
         foreach($installer in $installArray) {
             $count++
-            Write-Progress -Activity "Installing..." -PercentComplete ($count / $installArray.Length * 100) -CurrentOperation "$installer"
+            Write-Progress -Activity 'Installing...' -Status $installer -PercentComplete ($count / $installArray.Length * 100) -CurrentOperation "$count of $($installArray.Length) complete"
             Start-Process -FilePath $installer -WorkingDirectory $installDirectory -Wait
         }
-        Write-Progress -Activity "Installing..." -Completed 
+        Write-Progress -Activity 'Installing...' -Status 'Complete' -Completed 
     }
 
     ### Update hostfile with items in hostinfo.txt
-    Write-Host "Updating host file"
+    Write-Host 'Updating host file'
     $hostPath = "$env:windir\System32\drivers\etc\hosts"
     $hostinfo = Get-Content .\hostinfo.txt
     Add-Content -Path $hostPath -Value $hostinfo
@@ -79,7 +79,7 @@ if (!$update) {
 else {
     ### Update flag was set
     ### Run ninite and choco updates
-    $niniteExe = Get-ChildItem -Path $HOME\Downloads\ -Filter "Ninite" | Sort-Object LastAccessTime -Descending | Select-Object -First 1
+    $niniteExe = Get-ChildItem -Path $HOME\Downloads\ -Filter 'Ninite' | Sort-Object LastAccessTime -Descending | Select-Object -First 1
     if ($niniteExe) { 
         Start-Process -FilePath $niniteExe.FullName
     }
